2048155719188daef8154fe365d57f771e1284ab
Kauã Valim <kaua_a.val@outlook.com>
2024-06-22 22:00:04 -0300

    Desenvolvimento 22/06/2024 incremento
    Início da aplicação de notícias no site

--- cadastro.php
+++ cadastro.php
@@ -11,7 +11,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             $email = $_POST["email"];
             $senha = $_POST["senha"];
             $usuario->registrar($nome, $sexo, $fone, $email, $senha);
-            header("location:index.php");
+            header("location:login.php");
             exit();
         } catch (PDOException $exception) {
             $mensagem_erro = "Já existe um cadastro com este e-mail, tente novamente.";
---
+++ classes/Noticias.php
@@ -0,0 +1,46 @@
+<?php
+    class Noticias {
+
+        private $conn;
+        private $table_name="noticias"; // Nome da tabela
+
+        public function __construct($db) {
+            $this->conn=$db;
+        }
+
+        public function registrar($idusu, $data, $titulo, $noticia) {
+            $query = "INSERT INTO ".$this->table_name." (idusu, data, titulo, noticia) VALUES (?, ?, ?, ?)";
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute([$idusu, $data, $titulo, $noticia]);
+            return $stmt;
+        }
+
+        public function atualizar($idnot, $idusu, $data, $titulo, $noticia) {
+            $query = "UPDATE ".$this->table_name." SET idusu=?,data=?,titulo=?,noticia=? WHERE idnot=?";
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute([$idusu, $data, $titulo, $noticia, $idnot]);
+            return $stmt;
+        }
+
+        public function deletar($idnot){
+            $query = "DELETE FROM ".$this->table_name." WHERE idnot=?";
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute([$idnot]);
+            return $stmt;
+        }
+
+        public function ler(){
+            $query = "SELECT * FROM ".$this->table_name;
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute();
+            return $stmt;
+        }
+
+        public function lerPorId($idnot){
+            $query = "SELECT * FROM ".$this->table_name." WHERE idnot=?";
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute([$idnot]);
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        }
+        
+    }
